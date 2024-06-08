d9d89fb2a041785ac32028789cc64d6f0b02e3cc
Martin Ståhl <mrtnsthl@gmail.com>
2024-06-08 17:31:33 +0200

    Progress

--- goodly_weather_station/goodly_weather_station.ino
+++ goodly_weather_station/goodly_weather_station.ino
@@ -1,12 +1,14 @@
 //----------------------------------------------------------------------
 // https://github.com/clearwater/SwitecX25
-// 
+//
 
 //----------------------------------------------------------------------
 
 #include <SD.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <WiFi.h>
+#include <time.h>
 #include <DHT22.h>
 #include <DS18B20.h>
 #include <Adafruit_GFX.h>
@@ -17,7 +19,7 @@
 
 
 // STATES
-enum DisplayState {
+enum class DisplayState {
   INIT,
   OFF,
   SENSORS,
@@ -28,8 +30,28 @@ enum DisplayState {
   MESSAGES
 };
 
-DisplayState displayState = INIT;
-//DisplayState displayStatePrev = INIT;
+DisplayState displayState = DisplayState::INIT;
+DisplayState displayStatePrev = DisplayState::INIT;
+
+enum class DialState {
+  INIT,
+  TEMPERATURE_IN,
+  TEMPERATURE_OUT,
+  PRESSURE,
+  HUMIDITY
+};
+
+DialState dialStateTop = DialState::INIT;
+DialState dialStateBottom = DialState::INIT;
+
+// DIAL LIMITS
+#define DIAL_TEMPERATURE_MAX 50
+#define DIAL_TEMPERATURE_MIN -50
+#define DIAL_PRESSURE_MAX 1200
+#define DIAL_PRESSURE_MIN 800
+#define DIAL_HUMIDITY_MAX 100
+#define DIAL_HUMIDITY_MIN 0
+
 
 // PINS
 #define PIN_SPI_DC 9
@@ -44,20 +66,23 @@ DisplayState displayState = INIT;
 #define MOTOR1_PIN4 A7
 #define MOTOR2_PIN1 A0
 #define MOTOR2_PIN2 A1
-#define MOTOR2_PIN3 A2
-#define MOTOR2_PIN4 A3
+#define MOTOR2_PIN3 A1
+#define MOTOR2_PIN4 A2
 
 #define PIN_ENCODER_CLK 5
 #define PIN_ENCODER_DT 4
-#define PIN_ENCODER_SW 3 
+#define PIN_ENCODER_SW 3
 
-#define PIN_DS18B20 5   // ESP32 GPIO pin due to OneWire.h implementation
+#define PIN_DS18B20 5  // ESP32 GPIO pin due to OneWire.h implementation
+
 
 // USER DEFINES
 #define DISPLAY_OFF_SEC 60
+#define DISPLAY_MAX_LINES 8 // For font size 1
 #define SENSOR_READ_INTERV_SEC 5
 #define SENSOR_READ_INPUT_DLY_SEC 5
 #define ENCODER_DEBOUNCE_MILLIS 5
+
 
 // ROTARY ENCODER PUSH BUTTON
 //detachInterrupt(GPIOPin);
@@ -67,14 +92,27 @@ volatile bool encoder_ccw_pressed = false;
 volatile bool encoder_push_pressed = false;
 volatile int encoder_position = 0;
 
+
+// USER CONSTANTS
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 0;
+
+
 // USER VARIABLES
 volatile unsigned long lastInputMillis = millis();
 unsigned long lastSensorReadMillis = 0;
+float temperature_in;
+float temperature_out;
+float pressure;
+float humidity_rel;
+unsigned int currentMessageLine = 0;
+String messages[DISPLAY_MAX_LINES];
+
 
 // DHT22 TEMP AND HUMIDITY SENSOR
 DHT22 dht22(PIN_DHT);
-int dht_temperature;
-int dht_humidity_rel;
+float dht_temperature;
+float dht_humidity_rel;
 
 
 // BMP180 PRESSURE (AND TEMP) SENSOR (DEFAULT I2C PINS A4, A5)
@@ -82,115 +120,316 @@ Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 float bmp_temperature;
 float bmp_pressure;
 
+
 // DS18B20 1wire temp sensor
 DS18B20 ds(PIN_DS18B20);
-uint8_t ds_address[] = {40, 250, 31, 218, 4, 0, 0, 52};
+uint8_t ds_address[] = { 40, 250, 31, 218, 4, 0, 0, 52 };
 uint8_t ds_selected;
 float ds_temperature;
 
+
 // STEPPER MOTOR DEFINES
 // standard X25.168 range 315 degrees at 1/3 degree steps
-#define STEPS (315*3)
+//#define STEPS (315*3)
+//#define STEPS (360*3)
+#define DIAL_RANGE_DEG 216
+#define DIAL_RANGE_STEPS (DIAL_RANGE_DEG * 3)
 //SwitecX25 motor1(STEPS,9,7,5,3);
-SwitecX25 *motor1;
-SwitecX25 *motor2;
+SwitecX25* motor1;
+SwitecX25* motor2;
 
 
 //OLED DISPLAY DEFINES
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
-  &SPI, PIN_SPI_DC, PIN_OLED_RESET, PIN_SPI_CS_OLED);
+                         &SPI, PIN_SPI_DC, PIN_OLED_RESET, PIN_SPI_CS_OLED);
 bool displayOn = true;
 
-void setup()
-{
+
+void setup() {
   initSerial();
   initDisplay();
   initEncoder();
   initBmp();
   initDs();
   initSd();
+  initWifi();
   //initDht();
   initSteppers();
 }
 
 
-void loop()
-{
-  if (millis() - lastInputMillis > DISPLAY_OFF_SEC*1000) {
+void loop() {
+  if (millis() - lastInputMillis > DISPLAY_OFF_SEC * 1000) {
     setDisplayOff();
     displayOn = false;
-  }
-  else {
+  } else {
     displayOn = true;
   }
-  
-  //displayEncoderReadings();
 
-  if (encoder_push_pressed) {
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.print("Encoder push");
-    display.display();
-    encoder_push_pressed = false;
-    delay(1000);
-  }
-
-  if (encoder_cw_pressed) {
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.print("Encoder clockwise");
-    display.display();
-    delay(1000);
-    encoder_cw_pressed = false;
-  }
-
-  if (encoder_ccw_pressed) {
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.print("Encoder counter clockwise");
-    display.display();
-    delay(1000);
-    encoder_ccw_pressed = false;
-  }
-
-  if (millis() - lastSensorReadMillis > SENSOR_READ_INTERV_SEC*1000) {
+  if (millis() - lastSensorReadMillis > SENSOR_READ_INTERV_SEC * 1000) {
     readSensors();
   }
 
   if (displayOn) {
     printSensorReadings();
   }
-
-  /*
-  static int nextPos = 0;
-  // the motor only moves when you call update
-  motor1->update();
-  
-  if (Serial.available()) {
-    char c = Serial.read();
-    if (c==10 || c==13) {
-      motor1->setPosition(nextPos);
-      nextPos = 0;
-    } else if (c>='0' && c<='9') {
-      nextPos = 10*nextPos + (c-'0');
-    }
-  }
-  */
 }
 
 
-void testdrawcircle() {
-  display.clearDisplay();
+// INIT FUNCTIONS
 
-  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
+void initDisplay() {
+  // OLED DISPLAY INIT
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  digitalWrite(PIN_SPI_CS_SD, HIGH);  // set other SPI devices' CS high
+  if (!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println("SSD1306 allocation failed");
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  //display.display();
+  //delay(1000);
+  //testdrawcircle();
+  // Clear the buffer
+  display.clearDisplay();
+  display.display();
+  // Init text, enough to keep here?
+  display.setTextSize(1);               // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.cp437(true);                  // Use full 256 char 'Code Page 437' font
+}
+
+
+void initSd() {
+  //SD CARD READER
+  digitalWrite(PIN_SPI_CS_OLED, HIGH);
+  displayMessage("Initializing SD card");
+  delay(500);
+  if (!SD.begin(PIN_SPI_CS_SD)) {
+    displayMessage("SD card failed or not present");
+    delay(3000);
+  } else {
+    listFiles(SD.open("/"), 0);
+    displayMessage("SD card initialized");
+    delay(3000);
+    // File file = SD.open("/wifi.txt", FILE_WRITE);
+    // if (file) {
+    //   Serial.println("Writing to wifi.txt...");
+    //   // Write two lines to the file
+    //   file.println("ssid");
+    //   file.println("password");
+    //   // Close the file
+    //   file.close();
+    //   Serial.println("Done.");
+    // } else {
+    //   Serial.println("Error opening file.");
+    // }
+  }
+}
+
+
+void initEncoder() {
+  // ROTARY ENCODER INTERRUPT
+  pinMode(PIN_ENCODER_CLK, INPUT);
+  pinMode(PIN_ENCODER_DT, INPUT);
+  pinMode(PIN_ENCODER_SW, INPUT_PULLUP);
+  attachInterrupt(PIN_ENCODER_CLK, isrEncoderClock, CHANGE);
+  attachInterrupt(PIN_ENCODER_SW, isrEncoderPush, RISING);
+}
+
+
+void initBmp() {
+  // BMP180 INIT
+  /* Initialise the sensor */
+  displayMessage("Initializing BMP085");
+  delay(500);
+  if (!bmp.begin()) {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    displayMessage("No BMP085 detected, check I2C");
+    delay(3000);
+    //while(1);
+  }
+}
+
+
+void initSerial() {
+  Serial.begin(115200);
+  delay(1000);
+  //while (!Serial) {}
+}
+
+
+void initDs() {
+  //DS18B20 INIT
+  displayMessage("Initializing DS18B20");
+  delay(500);
+  ds_selected = ds.select(ds_address);
+}
+
+
+void initSteppers() {
+  // STEPPER MOTOR INIT
+  // Setting pin mode in SwitecX25 constructor before setup() does not work
+  // Create stepper object here
+
+  displayMessage("Initializing steppers");
+  delay(500);
+  motor1 = new SwitecX25(DIAL_RANGE_STEPS, MOTOR1_PIN1, MOTOR1_PIN2, MOTOR1_PIN3, MOTOR1_PIN4);
+  motor2 = new SwitecX25(DIAL_RANGE_STEPS, MOTOR2_PIN1, MOTOR2_PIN2, MOTOR2_PIN3, MOTOR2_PIN4);
+
+  //display.clearDisplay();
+  //display.setCursor(0, 0);
+
+  displayMessage("Please center top dial");
+  zeroStepper(motor1);
+
+  displayMessage("Please center bottom dial");
+  zeroStepper(motor2);
+
+  // motor1->setPosition(0);
+  // motor1->updateBlocking();
+  // motor1->setPosition(DIAL_RANGE_STEPS/2);
+  // motor1->updateBlocking();
+  // motor2->setPosition(0);
+  // motor2->updateBlocking();
+  // motor2->setPosition(DIAL_RANGE_STEPS/2);
+  // motor2->updateBlocking();
+}
+
+
+// INTERRUPT FUNCTIONS
+
+void isrEncoderClock() {
+  //if (millis() - lastInputMillis < ENCODER_DEBOUNCE_MILLIS)
+  bool encoder_clock = digitalRead(PIN_ENCODER_CLK);
+  if (encoder_clock != encoder_clock_prev) {
+    bool encoder_dt = digitalRead(PIN_ENCODER_DT);
+    if (encoder_dt != encoder_clock) {
+      //encoder_cw_pressed = true;
+      encoder_position++;
+    } else {
+      //encoder_ccw_pressed = true;
+      encoder_position--;
+    }
+    encoder_clock_prev = encoder_clock;
+  }
+  lastInputMillis = millis();
+}
+
+
+// IRAM_ATTR?
+void isrEncoderPush() {
+  encoder_push_pressed = true;
+  lastInputMillis = millis();
+}
+
+
+// OTHER FUNCTIONS
+
+void zeroStepper(SwitecX25* motor) {
+  int encoder_position_prev = encoder_position;
+  encoder_push_pressed = false;
+  while (!encoder_push_pressed) {
+    if (encoder_position > encoder_position_prev) {
+      motor->resetCenterPos();
+      motor->setPosition(DIAL_RANGE_STEPS / 2 + 2);
+      encoder_position_prev = encoder_position;
+    } else if (encoder_position < encoder_position_prev) {
+      motor->resetCenterPos();
+      motor->setPosition(DIAL_RANGE_STEPS / 2 - 2);
+      encoder_position_prev = encoder_position;
+    }
+    motor->update();
+  }
+  motor->resetCenterPos();
+}
+
+
+void updateDial(SwitecX25* motor, DialState state) {
+  switch (state) {
+    case DialState::TEMPERATURE_IN:
+      //position = valToDialPos(temperature_in, DIAL_TEMPERATURE_MAX, DIAL_TEMPERATURE_MIN);
+      //motor->setPosition(position);
+      break;
+    case DialState::TEMPERATURE_OUT:
+      break;
+    case DialState::PRESSURE:
+      break;
+    case DialState::HUMIDITY:
+      break;
+  }
+  motor->updateBlocking();
+}
+
+
+unsigned int valToDialPos(float val, float max, float min) {
+}
+
+
+void displayMessage(const String& message) {
+
+  // Change display state?
+  if (displayState != DisplayState::MESSAGES) {
+    displayState = DisplayState::MESSAGES;
   }
 
-  delay(2000);
+  // If buffer is full, shift all lines up
+  if (currentMessageLine >= DISPLAY_MAX_LINES) {
+    for (int i = 1; i < DISPLAY_MAX_LINES; i++) {
+      messages[i - 1] = messages[i];
+    }
+    currentMessageLine--;
+  }
+
+  // Add new line to buffer
+  messages[currentMessageLine] = message;
+  currentMessageLine++;
+
+  // Redraw display
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  for (int i = 0; i < currentMessageLine; i++) {
+    display.setCursor(0, i * 8); // Each line is 8 pixels tall
+    display.println(messages[i]);
+  }
+
+  display.display();
+
+  Serial.println(message);
+}
+
+
+void displayEncoderReadings() {
+  bool CLK = true;
+  bool DT = true;
+  bool SW = true;
+  display.clearDisplay();
+  display.println("CLK, DT, SW:");
+  display.display();
+  delay(1000);
+  for (;;) {
+    CLK = digitalRead(PIN_ENCODER_CLK);
+    DT = digitalRead(PIN_ENCODER_DT);
+    SW = digitalRead(PIN_ENCODER_SW);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Hej");
+    display.println(CLK);
+    display.println(DT);
+    display.println(SW);
+    display.display();
+  }
+}
+
+
+void setDisplayOff() {
+  display.clearDisplay();
+  display.display();
 }
 
 
@@ -201,9 +440,11 @@ void readBmp() {
   bmp.getTemperature(&bmp_temperature);
 }
 
+
 void readDs() {
   ds_temperature = ds.getTempC();
 }
+
 
 void readDht() {
   dht_temperature = dht22.getTemperature();
@@ -222,7 +463,7 @@ void readSensors() {
 
 void printSensorReadings() {
   display.clearDisplay();
-  display.setCursor(0, 0);     // Start at top-left corner
+  display.setCursor(0, 0);  // Start at top-left corner
 
   display.print("Temp bmp: ");
   display.print(bmp_temperature);
@@ -255,158 +496,112 @@ void printSensorReadings() {
 }
 
 
-void isrEncoderClock() {
-  //if (millis() - lastInputMillis < ENCODER_DEBOUNCE_MILLIS)
-  bool encoder_clock = digitalRead(PIN_ENCODER_CLK);
-  if (encoder_clock != encoder_clock_prev) {
-    bool encoder_dt = digitalRead(PIN_ENCODER_DT);
-    if (encoder_dt != encoder_clock) {
-      //encoder_cw_pressed = true;
-      encoder_position++;
+void initWifi() {
+  String password;
+  String ssid;
+  displayMessage("Initializing WiFi");
+  File file = SD.open("/wifi.txt");
+  if (file) {
+    if (file.available()) {
+      ssid = file.readStringUntil('\n');
+      ssid.trim();
+      //ssid = line.c_str();
+      displayMessage(ssid);
     }
-    else {
-      //encoder_ccw_pressed = true;
-      encoder_position--;
+    if (file.available()) {
+      password = file.readStringUntil('\n');
+      password.trim();
+      //password = line.c_str();
+      displayMessage(password);
     }
-    encoder_clock_prev = encoder_clock;
+  } else {
+    displayMessage("Could not open /wifi.txt");
+    return;
   }
-  lastInputMillis = millis();
-}
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  // Will try for about 10 seconds (20x 500ms)
+  int tryDelay = 500;
+  int numberOfTries = 20;
 
+  // Wait for the WiFi event
+  while (true) {
+    switch (WiFi.status()) {
+      case WL_NO_SSID_AVAIL:
+        Serial.println("[WiFi] SSID not found");
+        break;
+      case WL_CONNECT_FAILED:
+        Serial.print("[WiFi] Failed - WiFi not connected! Reason: ");
+        return;
+        break; 
+      case WL_CONNECTION_LOST:
+        Serial.println("[WiFi] Connection was lost");
+        break;
+      case WL_SCAN_COMPLETED:
+        Serial.println("[WiFi] Scan is completed");
+        break;
+      case WL_DISCONNECTED:
+        Serial.println("[WiFi] WiFi is disconnected");
+        break;
+      case WL_CONNECTED:
+        Serial.println("[WiFi] WiFi is connected!");
+        Serial.print("[WiFi] IP address: ");
+        Serial.println(WiFi.localIP());
+        return;
+        break;
+      default:
+        Serial.print("[WiFi] WiFi Status: ");
+        Serial.println(WiFi.status());
+        break;
+    }
+    delay(tryDelay);
 
-// IRAM_ATTR?
-void isrEncoderPush() {
-  encoder_push_pressed = true;
-  lastInputMillis = millis();
-}
-
-
-void displayEncoderReadings() {
-  bool CLK = true;
-  bool DT = true;
-  bool SW = true;
-  display.clearDisplay();
-  display.println("CLK, DT, SW:");
-  display.display();
-  delay(1000);  
-  for (;;) {
-    CLK = digitalRead(PIN_ENCODER_CLK);
-    DT = digitalRead(PIN_ENCODER_DT);
-    SW = digitalRead(PIN_ENCODER_SW);
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.println("Hej");
-    display.println(CLK);
-    display.println(DT);
-    display.println(SW);
-    display.display();
-  }
-}
-
-void setDisplayOff() {
-  display.clearDisplay();
-  display.display();
-}
-
-void initDisplay() {
-  // OLED DISPLAY INIT
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  digitalWrite(PIN_SPI_CS_SD, HIGH);  // set other SPI devices' CS high
-  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
-    Serial.println("SSD1306 allocation failed");
-    for(;;); // Don't proceed, loop forever
-  }
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(1000); 
-  //testdrawcircle();
-  // Clear the buffer
-  display.clearDisplay();
-  display.display();
-  // Init text, enough to keep here?
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-}
-
-void initSd() {
-  //SD CARD READER
-  digitalWrite(PIN_SPI_CS_OLED, HIGH);
-  displayMessage("Initializing SD card");
-  delay(3000);
-  if (!SD.begin(PIN_SPI_CS_SD)) {
-    displayMessage("SD card failed or not present");
-    delay(3000);
-  }
-  else {
-    //displayMessage("SD card initialized");
-    //delay(3000);
+    if (numberOfTries <= 0) {
+      Serial.println("[WiFi] Failed to connect to WiFi!");
+      // Use disconnect function to force stop trying to connect
+      WiFi.disconnect();
+      return;
+    } else {
+      numberOfTries--;
+    }
   }
 }
 
-void displayMessage(const String& message) {
-  if (displayState != MESSAGES) {
-    displayState = MESSAGES;
-    display.clearDisplay();
-    display.setCursor(0, 0);
-  }
-  display.println(message);
-  display.display();
-  Serial.println(message);
-}
 
-void initEncoder() {
-  // ROTARY ENCODER INTERRUPT
-  pinMode(PIN_ENCODER_CLK, INPUT);
-  pinMode(PIN_ENCODER_DT, INPUT);
-  pinMode(PIN_ENCODER_SW, INPUT_PULLUP);
-  attachInterrupt(PIN_ENCODER_CLK, isrEncoderClock, CHANGE);
-  attachInterrupt(PIN_ENCODER_SW, isrEncoderPush, RISING);
-}
-
-void initBmp() {
-   // BMP180 INIT
-  /* Initialise the sensor */
-  displayMessage("Initializing BMP085");
-  delay(1000);
-  if(!bmp.begin())
-  {
-    /* There was a problem detecting the BMP085 ... check your connections */
-    displayMessage("No BMP085 detected, check I2C");
-    delay(3000);
-    //while(1);
+void listFiles(File dir, int numTabs) {
+  while (true) {
+    File entry = dir.openNextFile();
+    if (!entry) {
+      // No more files
+      break;
+    }
+    for (unsigned int i = 0; i < numTabs; i++) {
+      Serial.print('\t');  // Print tabs for nested files
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      listFiles(entry, numTabs + 1);  // Recursively list subdirectories
+    } else {
+      // Files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
   }
 }
 
-void initSerial() {
-  Serial.begin(115200);
-  delay(1000);
-  //while (!Serial) {}
-}
+// void updateDisplay() {
+//   switch(displayState) {
+//     case MENU_ZERO_MOTOR_1:
 
-void initDs() {
-  //DS18B20 INIT
-  displayMessage("Initializing DS18B20");
-  delay(1000);
-  ds_selected = ds.select(ds_address);
-}
-
-void initSteppers() {
-    // STEPPER MOTOR INIT
-  // Setting pin mode in SwitecX25 constructor before setup() does not work
-  // Create stepper object here
-  displayMessage("Initializing steppers");
-  delay(1000);
-  motor1 = new SwitecX25(STEPS, MOTOR1_PIN1, MOTOR1_PIN2, MOTOR1_PIN3, MOTOR1_PIN4);
-  motor2 = new SwitecX25(STEPS, MOTOR2_PIN1, MOTOR2_PIN2, MOTOR2_PIN3, MOTOR2_PIN4);
-  // run the motor against the stops
-  //motor1->zero();
-  //motor2->zero();
-  // start moving towards the center of the range
-  //motor1->setPosition(STEPS/2);
-  //motor2->setPosition(STEPS/2);
-  //Serial.print("Enter a step position from 0 through ");
-  //Serial.print(STEPS-1);
-  //Serial.println(".");
-}
+//   }
+//   //   INIT,
+//   // OFF,
+//   // SENSORS,
+//   // MENU,
+//   // MENU_SET_TIME,
+//   // MENU_ZERO_MOTOR_1,
+//   // MENU_ZERO_MOTOR_2,
+//   // MESSAGES
+// }
--- goodly_weather_station/src/SwitecX25/SwitecX25.cpp
+++ goodly_weather_station/src/SwitecX25/SwitecX25.cpp
@@ -11,9 +11,9 @@
 
 #include "SwitecX25.h"
 
-// During zeroing we will step the motor CCW 
+// During zeroing we will step the motor CCW
 // with a fixed step period defined by RESET_STEP_MICROSEC
-//#define RESET_STEP_MICROSEC 800
+// #define RESET_STEP_MICROSEC 800
 #define RESET_STEP_MICROSEC 2000
 
 // This table defines the acceleration curve as a list of (step, delay) pairs.
@@ -21,18 +21,17 @@
 // 1st value in each subsequent row must be > 1st value in previous row
 // The delay in the last row determines the maximum angular velocity.
 static unsigned short defaultAccelTable[][2] = {
-  {   20, 30000},
-  {   50, 15000},
-  {  100, 10000},
-  {  150,  8000},
-  {  300,  6000}
-};
-#define DEFAULT_ACCEL_TABLE_SIZE (sizeof(defaultAccelTable)/sizeof(*defaultAccelTable))
+    {20, 30000},
+    {50, 15000},
+    {100, 10000},
+    {150, 8000},
+    {300, 6000}};
+#define DEFAULT_ACCEL_TABLE_SIZE (sizeof(defaultAccelTable) / sizeof(*defaultAccelTable))
 
-// experimentation suggests that 400uS is about the step limit 
+// experimentation suggests that 400uS is about the step limit
 // with my hand-made needles made by cutting up aluminium from
 // floppy disk sliders.  A lighter needle will go faster.
-  
+
 // State  3 2 1 0   Value
 // 0      1 0 0 1   0x9
 // 1      0 0 0 1   0x1
@@ -50,25 +49,27 @@ SwitecX25::SwitecX25(unsigned int steps, unsigned char pin1, unsigned char pin2,
   this->pins[1] = pin2;
   this->pins[2] = pin3;
   this->pins[3] = pin4;
-  for (int i=0;i<pinCount;i++) {
+  for (int i = 0; i < pinCount; i++)
+  {
     pinMode(pins[i], OUTPUT);
   }
-  
+
   dir = 0;
-  vel = 0; 
+  vel = 0;
   stopped = true;
   currentStep = 0;
   targetStep = 0;
 
   accelTable = defaultAccelTable;
-  maxVel = defaultAccelTable[DEFAULT_ACCEL_TABLE_SIZE-1][0]; // last value in table.
+  maxVel = defaultAccelTable[DEFAULT_ACCEL_TABLE_SIZE - 1][0]; // last value in table.
 }
 
 void SwitecX25::writeIO()
 {
 
-  byte mask = stateMap[currentState];  
-  for (int i=0;i<pinCount;i++) {
+  byte mask = stateMap[currentState];
+  for (int i = 0; i < pinCount; i++)
+  {
     digitalWrite(pins[i], mask & 0x1);
     mask >>= 1;
   }
@@ -76,7 +77,8 @@ void SwitecX25::writeIO()
 
 void SwitecX25::stepUp()
 {
-  if (currentStep < steps) {
+  if (currentStep < steps)
+  {
     currentStep++;
     currentState = (currentState + 1) % stateCount;
     writeIO();
@@ -84,8 +86,9 @@ void SwitecX25::stepUp()
 }
 
 void SwitecX25::stepDown()
-{ 
-  if (currentStep > 0) {
+{
+  if (currentStep > 0)
+  {
     currentStep--;
     currentState = (currentState + 5) % stateCount;
     writeIO();
@@ -95,7 +98,8 @@ void SwitecX25::stepDown()
 void SwitecX25::zero()
 {
   currentStep = steps - 1;
-  for (unsigned int i=0;i<steps;i++) {
+  for (unsigned int i = 0; i < steps; i++)
+  {
     stepDown();
     delayMicroseconds(RESET_STEP_MICROSEC);
   }
@@ -105,8 +109,13 @@ void SwitecX25::zero()
   dir = 0;
 }
 
+void SwitecX25::resetCenterPos()
+{
+  currentStep = steps / 2;
+}
+
 // This function determines the speed and accel
-// characteristics of the motor.  Ultimately it 
+// characteristics of the motor.  Ultimately it
 // steps the motor once (up or down) and computes
 // the delay until the next step.  Because it gets
 // called once per step per motor, the calcuations
@@ -117,56 +126,70 @@ void SwitecX25::zero()
 // velocity as the number of motor steps travelled under acceleration
 // since starting.  This value is used to look up the corresponding
 // delay in accelTable.  So from a standing start, vel is incremented
-// once each step until it reaches maxVel.  Under deceleration 
+// once each step until it reaches maxVel.  Under deceleration
 // vel is decremented once each step until it reaches zero.
 
 void SwitecX25::advance()
 {
   // detect stopped state
-  if (currentStep==targetStep && vel==0) {
+  if (currentStep == targetStep && vel == 0)
+  {
     stopped = true;
     dir = 0;
     time0 = micros();
     return;
   }
-  
+
   // if stopped, determine direction
-  if (vel==0) {
-    dir = currentStep<targetStep ? 1 : -1;
+  if (vel == 0)
+  {
+    dir = currentStep < targetStep ? 1 : -1;
     // do not set to 0 or it could go negative in case 2 below
-    vel = 1; 
+    vel = 1;
   }
-  
-  if (dir>0) {
+
+  if (dir > 0)
+  {
     stepUp();
-  } else {
+  }
+  else
+  {
     stepDown();
   }
-  
+
   // determine delta, number of steps in current direction to target.
   // may be negative if we are headed away from target
-  int delta = dir>0 ? targetStep-currentStep : currentStep-targetStep;
-  
-  if (delta>0) {
+  int delta = dir > 0 ? targetStep - currentStep : currentStep - targetStep;
+
+  if (delta > 0)
+  {
     // case 1 : moving towards target (maybe under accel or decel)
-    if (delta < vel) {
+    if (delta < vel)
+    {
       // time to declerate
       vel--;
-    } else if (vel < maxVel) {
+    }
+    else if (vel < maxVel)
+    {
       // accelerating
       vel++;
-    } else {
+    }
+    else
+    {
       // at full speed - stay there
     }
-  } else {
+  }
+  else
+  {
     // case 2 : at or moving away from target (slow down!)
     vel--;
   }
-    
+
   // vel now defines delay
   unsigned char i = 0;
   // this is why vel must not be greater than the last vel in the table.
-  while (accelTable[i][0]<vel) {
+  while (accelTable[i][0] < vel)
+  {
     i++;
   }
   microDelay = accelTable[i][1];
@@ -176,9 +199,11 @@ void SwitecX25::advance()
 void SwitecX25::setPosition(unsigned int pos)
 {
   // pos is unsigned so don't need to check for <0
-  if (pos >= steps) pos = steps-1;
+  if (pos >= steps)
+    pos = steps - 1;
   targetStep = pos;
-  if (stopped) {
+  if (stopped)
+  {
     // reset the timer to avoid possible time overflow giving spurious deltas
     stopped = false;
     time0 = micros();
@@ -188,23 +213,25 @@ void SwitecX25::setPosition(unsigned int pos)
 
 void SwitecX25::update()
 {
-  if (!stopped) {
+  if (!stopped)
+  {
     unsigned long delta = micros() - time0;
-    if (delta >= microDelay) {
+    if (delta >= microDelay)
+    {
       advance();
     }
   }
 }
 
-
-//This updateMethod is blocking, it will give you smoother movements, but your application will wait for it to finish
+// This updateMethod is blocking, it will give you smoother movements, but your application will wait for it to finish
 void SwitecX25::updateBlocking()
 {
-  while (!stopped) {
+  while (!stopped)
+  {
     unsigned long delta = micros() - time0;
-    if (delta >= microDelay) {
+    if (delta >= microDelay)
+    {
       advance();
     }
   }
 }
-
